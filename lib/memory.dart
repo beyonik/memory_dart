2ec0385ed7d1bbf17fd2afe73efe38be88ece329
beyonik <amacaputi@gmail.com>
2022-06-17 13:17:12 +0300

    export

--- lib/memory.dart
+++ lib/memory.dart
@@ -3,6 +3,9 @@ import 'dart:ffi';
 import 'package:memory_dart/util/dylib_wrapper.dart';
 import 'package:memory_dart/util/win_path.dart';
 
+export 'package:memory_dart/process/process.dart';
+export 'package:memory_dart/memory/memory_access.dart';
+
 class Memory {
   static DynamicLibrary? _kernel32;
   static DynamicLibrary? get kernel32 => _kernel32;
